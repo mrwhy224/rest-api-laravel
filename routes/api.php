75813d2e7c6c1a28931a0beadd0cb34603dd1ab6
mrwhy224 <mail@mroohi.ir>
2024-03-28 14:57:26 +0330

    add looger part to api

---
+++ routes/api.php
@@ -0,0 +1,15 @@
+<?php
+
+use App\Http\Middleware\TokenIsValid;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+//Route::get('/user', function (Request $request) {
+//    return $request->user();
+//})->middleware('auth:sanctum');
+
+
+Route::group(['prefix'=>'v1', 'namespace'=>'App\Http\Controllers\api\v1', 'middleware' => ['App\Http\Middleware\TokenIsValid', 'App\Http\Middleware\RequestLogger']], function () {
+    Route::get('/account', [\App\Http\Controllers\api\v1\AccountController::class, 'index']);
+    Route::any('/account/store', [\App\Http\Controllers\api\v1\AccountController::class, 'store']);
+});
