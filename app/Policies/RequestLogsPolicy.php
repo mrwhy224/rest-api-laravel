52579e9811abea9d8cf209276ce1d637bf3c236d
mrwhy224 <mail@mroohi.ir>
2024-03-29 04:48:09 +0330

    comment errors and unnecessary functions

--- app/Policies/RequestLogsPolicy.php
+++ app/Policies/RequestLogsPolicy.php
@@ -10,57 +10,44 @@ class RequestLogsPolicy
 {
     /**
      * Determine whether the user can view any models.
-     */
+
     public function viewAny(User $user): bool
     {
         //
     }
 
-    /**
-     * Determine whether the user can view the model.
-     */
+
     public function view(User $user, RequestLogs $requestLogs): bool
     {
         //
     }
 
-    /**
-     * Determine whether the user can create models.
-     */
+
     public function create(User $user): bool
     {
         //
     }
 
-    /**
-     * Determine whether the user can update the model.
-     */
     public function update(User $user, RequestLogs $requestLogs): bool
     {
         //
     }
 
-    /**
-     * Determine whether the user can delete the model.
-     */
+
     public function delete(User $user, RequestLogs $requestLogs): bool
     {
         //
     }
 
-    /**
-     * Determine whether the user can restore the model.
-     */
+
     public function restore(User $user, RequestLogs $requestLogs): bool
     {
         //
     }
 
-    /**
-     * Determine whether the user can permanently delete the model.
-     */
     public function forceDelete(User $user, RequestLogs $requestLogs): bool
     {
         //
     }
+     */
 }
