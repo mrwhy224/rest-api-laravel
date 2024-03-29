18180edebbe3b9fac54269da561ae128aefd69cc
mrwhy224 <mail@mroohi.ir>
2024-03-29 04:42:23 +0330

    Changing the token_hash check method and check unique_info too

--- app/Http/Middleware/TokenIsValid.php
+++ app/Http/Middleware/TokenIsValid.php
@@ -6,6 +6,8 @@ use App\Models\Device;
 use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Config;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Validator;
 use Symfony\Component\HttpFoundation\Response;
 
 class TokenIsValid
@@ -13,9 +15,14 @@ class TokenIsValid
     public function handle(Request $request, Closure $next)
     {
         // toDo check user, account and token
+        $validator = Validator::make($request->all(), ['unique_info' => 'required']);
+        if ($validator->fails())
+            return  response()->error(401,"The authentication information used is incorrect", $validator->messages()->getMessages());
 
-        if(Device::where('token_hash', $request->header(Config::get('app.token_header_name')))->count()==1)
+
+        $dev = Device::where('unique_info', $request->unique_info)->first();
+        if ($dev && Hash::check($request->header(Config::get('app.token_header_name')), $dev->token_hash))
             return $next($request);
-        return  response()->error(401,"token is invalid", ["token is invalid"]);
+        return  response()->error(401,"The authentication information used is incorrect");
     }
 }
