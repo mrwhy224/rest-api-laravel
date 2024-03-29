76d6bdf7ca0cff9fd6c0e79149bc2a2c35d09f20
mrwhy224 <mail@mroohi.ir>
2024-03-29 04:45:05 +0330

    add $fillable variable

--- app/Models/Device.php
+++ app/Models/Device.php
@@ -8,9 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Device extends Model
 {
     use HasFactory;
-
+    protected $fillable = ['user_id', 'token_hash', 'unique_info', 'device_info'];
     public function user(): \Illuminate\Database\Eloquent\Relations\BelongsTo
     {
+
         return $this->belongsTo(User::class);
     }
 }
